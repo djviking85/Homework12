75babfb3ca251e4d4ff223ab0912609d1db76803
Viking <djviking@yandex.ru>
2023-01-31 19:01:32 +0300

    ДЗ12

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Book warAndPeace = new Book("Tolstoi", "War and Peace", 1868);
-
-        warAndPeace.authorName = "Л. Н.Толстой";
+        Book neznaika = new Book("Носов", "Незнайка на луне", 1964);
         System.out.println("Автор - " + warAndPeace.authorName);
-
-        warAndPeace.bookName = "Война и Мир";
         System.out.println("Название - " + warAndPeace.bookName);
-
-        warAndPeace.publishingYear = 1867;
         System.out.println("Год - " + warAndPeace.publishingYear);
+
+        System.out.println("Автор - " + neznaika.authorName);
+        System.out.println("Название - " + neznaika.bookName);
+        System.out.println("Год - " + neznaika.publishingYear);
 
         Autor tolstoi = new Autor();
 
